bc4e2b831cbe19590a5b6910dd1e0fb379ba26cd
01zulfi <85733202+01zulfi@users.noreply.github.com>
2024-05-19 17:13:28 +0500

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -12,43 +12,53 @@ func main() {
 	fmt.Println("Logs from your program will appear here!")
 
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
+
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+
+	defer l.Close()
+
+	for {
+		conn, err := l.Accept()
+
+		if err != nil {
+			fmt.Println("Error accepting connection: ", err.Error())
+			os.Exit(1)
+		}
+
+		go func() {
+			buff := make([]byte, 2048)
+			_, err = conn.Read(buff)
+
+			if err != nil {
+				fmt.Println("Error reading request: ", err.Error())
+				os.Exit(1)
+			}
+
+			request := string(buff)
+			requestTarget := strings.Split(request, " ")[1]
+			headers := strings.Split(request, "\r\n")[1:]
+
+			var response string
+
+			if requestTarget == "/" {
+				response = "HTTP/1.1 200 OK\r\n\r\n"
+			} else if strings.HasPrefix(requestTarget, "/echo") {
+				echoString := strings.Split(requestTarget, "/echo/")[1]
+				response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoString), echoString)
+
+			} else if strings.HasPrefix(requestTarget, "/user-agent") {
+				userAgentIndex := slices.IndexFunc(headers, func(h string) bool { return strings.Contains(h, "User-Agent: ") })
+				userAgent := strings.TrimPrefix(headers[userAgentIndex], "User-Agent: ")
+				response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
+			} else {
+				response = "HTTP/1.1 404 Not Found\r\n\r\n"
+			}
+
+			conn.Write([]byte(response))
+			conn.Close()
+		}()
 	}
-
-	buff := make([]byte, 2048)
-	_, err = conn.Read(buff)
-
-	if err != nil {
-		fmt.Println("Error reading request: ", err.Error())
-		os.Exit(1)
-	}
-
-	request := string(buff)
-	requestTarget := strings.Split(request, " ")[1]
-	headers := strings.Split(request, "\r\n")[1:]
-
-	var response string
-	if requestTarget == "/" {
-		response = "HTTP/1.1 200 OK\r\n\r\n"
-	} else if strings.HasPrefix(requestTarget, "/echo") {
-		echoString := strings.Split(requestTarget, "/echo/")[1]
-		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(echoString), echoString)
-
-	} else if strings.HasPrefix(requestTarget, "/user-agent") {
-		userAgentIndex := slices.IndexFunc(headers, func(h string) bool { return strings.Contains(h, "User-Agent: ") })
-		userAgent := strings.TrimPrefix(headers[userAgentIndex], "User-Agent: ")
-		response = fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(userAgent), userAgent)
-	} else {
-		response = "HTTP/1.1 404 Not Found\r\n\r\n"
-	}
-
-	conn.Write([]byte(response))
-	conn.Close()
 }
